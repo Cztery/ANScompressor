1c0a3eef9b4269c8937e7f9027571c8dd54a3611
Marcin Kurkowski <kurkowski.mrcn@gmail.com>
2022-08-08 22:26:48 +0200

    added common definitions header

--- src/common.h
+++ src/common.h
@@ -3,9 +3,10 @@
 
 namespace anslib {
 typedef uint8_t AnsSymbol;
-typedef uint64_t AnsState;
+typedef uint32_t AnsState;
 typedef uint32_t AnsCountsType;
-const uint32_t PROB_BITS = 30;
+const uint32_t PROB_BITS = 14u;
 const uint32_t PROB_SCALE = 1u << PROB_BITS;
-const uint32_t STATE_LOWER_BOUND = 1u << PROB_BITS;
+const uint32_t STATE_LOWER_BOUND = 1u << 24;
+const uint32_t ANS_SIGNATURE = 0x13;
 }  // namespace anslib
